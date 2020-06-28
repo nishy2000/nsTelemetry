0fb592b3436416155e1a7d2d41905e01d84f6f47
nishy2000 <github@nishy-software.com>
2020-06-28 23:57:16 +0900

    chore: Migrate project file to sdk style

--- nsTelemetryAI/Properties/AssemblyInfo.cs
+++ nsTelemetryAI/Properties/AssemblyInfo.cs
@@ -12,12 +12,6 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("nsTelemetryAI")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("nishy software")]
-[assembly: AssemblyProduct("nsTelemetryAI")]
-[assembly: AssemblyCopyright("©2015, 2017, 2018 nishy software")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -28,16 +22,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("9982e4f1-aa48-4438-8f67-607b916e41e2")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.12070")]
-[assembly: AssemblyFileVersion("1.0.0.12070")]
