50db6bbe42ec55d81c54aaf07593aad90a2d90cd
nishy2000 <github@nishy-software.com>
2023-04-24 23:55:04 +0900

    chore: update script files to create nuget package

--- nsTelemetry/nuget/BuildPackage.cmd
+++ nsTelemetry/nuget/BuildPackage.cmd
@@ -1,7 +1,11 @@
 @ECHO OFF
-REM Copyright 2020, 2022 nishy software
-SETLOCAL enabledelayedexpansion
+REM Copyright 2020, 2021, 2022, 2023 nishy software
+
+SETLOCAL EnableDelayedExpansion
+
 SET BAT_FOLDER=%~d0%~p0
+
+SET EXIT_ERR=0
 SET DATE_TIME=%DATE:/=%_%TIME::=%
 SET DATE_TIME=%DATE_TIME: =0%
 SET SRC_FOLDER=%BAT_FOLDER%..
@@ -9,9 +13,39 @@ SET WORK_FOLDERNAME=Package_%DATE_TIME%
 SET WORK_FOLDER=%BAT_FOLDER%%WORK_FOLDERNAME%
 SET SIGNED_FOLER=%WORK_FOLDER%\signed
 SET UNSIGNED_FOLER=%WORK_FOLDER%\unsigned
-SET CERT_SUBJECT_NAME=nishy software
 
-COLOR F
+SET CERT_SUBJECT_NAME=nishy software
+SET TIME_STAMP_RFC3161_URL=http://timestamp.digicert.com/?alg=sha256
+
+SET NO_BUILD=
+SET NO_PAUSE=
+IF "x-%1"=="x-/?" GOTO HELP
+IF "x-%1"=="x-/h" GOTO HELP
+IF "x-%1"=="x--h" GOTO HELP
+IF "x-%1"=="x--help" GOTO HELP
+IF "x-%1" == "x---no-build" SET NO_BUILD=true& SHIFT
+IF "x-%1" == "x--no-build" SET NO_BUILD=true& SHIFT
+IF "x-%1" == "x-/no-build" SET NO_BUILD=true& SHIFT
+IF "x-%1" == "x---no-pause" SET NO_PAUSE=true& SHIFT
+IF "x-%1" == "x--no-pause" SET NO_PAUSE=true& SHIFT
+IF "x-%1" == "x-/no-pause" SET NO_PAUSE=true& SHIFT
+IF "x-%1" == "x---no-build" SET NO_BUILD=true& SHIFT
+IF "x-%1" == "x--no-build" SET NO_BUILD=true& SHIFT
+IF "x-%1" == "x-/no-build" SET NO_BUILD=true& SHIFT
+
+SET TARGET_CONFIG=Release
+IF NOT "x-%1" == "x-" SET TARGET_CONFIG=%1
+
+SET BIN_FOLDER=bin\%TARGET_CONFIG%
+SET OBJ_FOLDER=obj\%TARGET_CONFIG%
+SET RUNTIME_ID_FRM=net452
+SET RUNTIME_ID_NET=netstandard2.0
+
+PUSHD "%SRC_FOLDER%"
+
+MKDIR "%WORK_FOLDER%"
+IF ERRORLEVEL 1 GOTO ERROR
+
 IF NOT "x-%SIGNTOOL%" == "x-" SET SIGNTOOL=%SIGNTOOL:"=%
 ECHO signtool: %SIGNTOOL%
 IF NOT "x-%SIGNTOOL%" == "x-" (
@@ -24,36 +58,58 @@ IF NOT "x-%SIGNTOOL%" == "x-" (
     ECHO [93mIn order to sign automatically, Please SET SIGNTOOL=sign batch file path[0m
 )
 
-mkdir "%WORK_FOLDER%"
-PUSHD "%SRC_FOLDER%"
-
+REM ------------------------
 REM  Rebuild modules
+REM ------------------------
 ECHO =======================
 ECHO Rebuild modules
 ECHO -----------------------
-dotnet.exe build -c Release --no-incremental --no-dependencies nsTelemetry.csproj
-IF ERRORLEVEL 1 GOTO ERROR
+IF "x-%NO_BUILD%" == "x-" (
+	dotnet.exe build -c %TARGET_CONFIG% --no-incremental --no-dependencies nsTelemetry.csproj
+	IF ERRORLEVEL 1 GOTO ERROR
+) ELSE (
+	ECHO [93mSkip[0m: rebuild modules
+)
+:REBUILD_END
 
 
+REM ------------------------
 REM  Backup unsigned modules
+REM ------------------------
 ECHO =======================
 ECHO Backup unsigned modules
 ECHO -----------------------
 
-mkdir "%UNSIGNED_FOLER%"
+MKDIR "%UNSIGNED_FOLER%"
 
 REM  NishySoftware.Telemetry modules
-XCOPY /D /E "bin\Release" "%UNSIGNED_FOLER%\" > nul
+XCOPY /D /E "%BIN_FOLDER%" "%UNSIGNED_FOLER%\" > nul
 IF ERRORLEVEL 1 GOTO ERROR
 
-
+REM ------------------------
 REM  Sign modules
+REM ------------------------
 ECHO =======================
 ECHO Sign modules
 ECHO -----------------------
 SET SIGN_FILES=
-SET SIGN_FILES=%SIGN_FILES% "bin\Release\net452\nsTelemetry.dll"
-SET SIGN_FILES=%SIGN_FILES% "bin\Release\netstandard2.0\nsTelemetry.dll"
+SET SIGN_FILES=%SIGN_FILES% "%BIN_FOLDER%\%RUNTIME_ID_FRM%\nsTelemetry.dll"
+SET SIGN_FILES=%SIGN_FILES% "%BIN_FOLDER%\%RUNTIME_ID_NET%\nsTelemetry.dll"
+
+REM Not signing when including Debug in %TARGET_CONFIG%
+IF NOT "x-%TARGET_CONFIG%" == "x-%TARGET_CONFIG:Debug=%" (
+	ECHO [93mSkip[0m: sign module files because configuration is '%TARGET_CONFIG%'
+	GOTO SIGN_END
+)
+
+FOR %%i in (%SIGN_FILES%) do (
+    "%BAT_FOLDER%signtool.exe" verify  /all /a /tw /pa %%i
+    IF ERRORLEVEL 1 (
+        GOTO SIGN_START
+    )
+)
+ECHO [93mSkip[0m: All files are already signed.
+GOTO VERIFY_START
 
 :SIGN_START
 IF EXIST "%SIGNTOOL%" (
@@ -66,8 +122,10 @@ IF EXIST "%SIGNTOOL%" (
     PAUSE
     COLOR
 )
+
+:VERIFY_START
 FOR %%i in (%SIGN_FILES%) do (
-    echo verify: %%i
+    ECHO verify: %%i
     "%BAT_FOLDER%signtool.exe" verify  /all /a /tw /pa %%i
     IF ERRORLEVEL 1 (
         COLOR 6F
@@ -79,48 +137,59 @@ FOR %%i in (%SIGN_FILES%) do (
 :SIGN_END
 
 
+REM ------------------------
 REM  Backup signed modules
+REM ------------------------
 ECHO =====================
 ECHO Backup signed modules
 ECHO -----------------------
 
-mkdir "%SIGNED_FOLER%"
-mkdir "%SIGNED_FOLER%\net452"
-mkdir "%SIGNED_FOLER%\netstandard2.0"
+MKDIR "%SIGNED_FOLER%"
+IF ERRORLEVEL 1 GOTO ERROR
+MKDIR "%SIGNED_FOLER%\%RUNTIME_ID_FRM%"
+IF ERRORLEVEL 1 GOTO ERROR
+MKDIR "%SIGNED_FOLER%\%RUNTIME_ID_NET%"
+IF ERRORLEVEL 1 GOTO ERROR
 
 REM  NishySoftware.Telemetry modules
-COPY "bin\Release\net452\nsTelemetry.dll" "%SIGNED_FOLER%\net452\" >nul
+COPY "%BIN_FOLDER%\%RUNTIME_ID_FRM%\nsTelemetry.dll" "%SIGNED_FOLER%\%RUNTIME_ID_FRM%\" >nul
 IF ERRORLEVEL 1 GOTO ERROR
-COPY "bin\Release\netstandard2.0\nsTelemetry.dll" "%SIGNED_FOLER%\netstandard2.0\" >nul
+COPY "%BIN_FOLDER%\%RUNTIME_ID_NET%\nsTelemetry.dll" "%SIGNED_FOLER%\%RUNTIME_ID_NET%\" >nul
 IF ERRORLEVEL 1 GOTO ERROR
 
+REM ------------------------
 REM  Copy signed resource files to obj folder
+REM ------------------------
 ECHO =======================
 ECHO Copy signed resource files to obj folder
 ECHO -----------------------
 
 REM  NishySoftware.Telemetry modules
-SET COPY_SOURCE=bin\Release\net452
-SET COPY_DEST=obj\Release\net452
+SET COPY_SOURCE=%BIN_FOLDER%\%RUNTIME_ID_FRM%
+SET COPY_DEST=%OBJ_FOLDER%\%RUNTIME_ID_FRM%
 COPY "%COPY_SOURCE%\nsTelemetry.dll" "%COPY_DEST%\" >nul
 IF ERRORLEVEL 1 GOTO ERROR
 
-SET COPY_SOURCE=bin\Release\netstandard2.0
-SET COPY_DEST=obj\Release\netstandard2.0
+SET COPY_SOURCE=%BIN_FOLDER%\%RUNTIME_ID_NET%
+SET COPY_DEST=%OBJ_FOLDER%\%RUNTIME_ID_NET%
 COPY "%COPY_SOURCE%\nsTelemetry.dll" "%COPY_DEST%\" >nul
 IF ERRORLEVEL 1 GOTO ERROR
 
 
+REM ------------------------
 REM  Packaging
+REM ------------------------
 ECHO =======================
 ECHO Packaging
 ECHO -----------------------
 
-dotnet.exe pack -c Release --no-build -o "%WORK_FOLDER%" nsTelemetry.csproj
+dotnet.exe pack -c %TARGET_CONFIG% --no-build -o "%WORK_FOLDER%" nsTelemetry.csproj
 IF ERRORLEVEL 1 GOTO ERROR
 
 
+REM ------------------------
 REM  Backup packages
+REM ------------------------
 ECHO =======================
 ECHO Backup packages
 ECHO -----------------------
@@ -130,29 +199,73 @@ FOR %%i in (%WORK_FOLDER%\*.nupkg) do (
     MOVE "%%i" "%UNSIGNED_FOLER%\%%~ni_unsigned%%~xi" > nul
 )
 
+REM ------------------------
 REM  Sign packages
+REM ------------------------
 ECHO =======================
 ECHO Sign packages
 ECHO -----------------------
 
+REM Not signing when including Debug in %TARGET_CONFIG%
+IF NOT "x-%TARGET_CONFIG%" == "x-%TARGET_CONFIG:Debug=%" (
+	ECHO [93mSkip[0m: sign package files because configuration is '%TARGET_CONFIG%'
+	GOTO SIGN_PACKAGE_END
+)
+
 FOR %%i in ("%WORK_FOLDER%\signed\*.nupkg") do (
-    echo sign: %%~ni%%~xi
-    "%BAT_FOLDER%nuget.exe" sign "%%i" -Verbosity quiet -CertificateSubjectName "%CERT_SUBJECT_NAME%"  -Timestamper "http://sha256timestamp.ws.symantec.com/sha256/timestamp" 
+    ECHO sign: %%~ni%%~xi
+    "%BAT_FOLDER%nuget.exe" sign "%%i" -Verbosity quiet -CertificateSubjectName "%CERT_SUBJECT_NAME%" -Timestamper "%TIME_STAMP_RFC3161_URL%" 
     IF ERRORLEVEL 1 GOTO ERROR
+)
+:SIGN_PACKAGE_END
+
+GOTO END
+
+REM ------------------------
+REM :ERROR
+REM ------------------------
+:ERROR
+
+SET EXIT_ERR=%ERRORLEVEL%
+ECHO [41m[1mERROR occurred (ERRORLEVEL=%EXIT_ERR%)[0m
+IF "x-%NO_PAUSE%" == "x-" (
+REM COLOR C1
+ECHO.[41m&PAUSE&ECHO [0m
+REM COLOR F
 )
 
 GOTO END
 
-:ERROR
-ECHO ERROR occurred
-COLOR C1
-PAUSE
-COLOR F
+REM ------------------------
+REM :HELP
+REM ------------------------
+:HELP
 
+ECHO.
+ECHO Usage: %BAT_NAME%  [--no-build] [--no-pause] [ConfigName]
+ECHO   remarks: Default value of ConfigName is 'Release'
+ECHO.
+
+SET EXIT_ERR=
+
+GOTO END
+
+REM ------------------------
+REM :END
+REM ------------------------
 :END
-ECHO =======================
-ECHO Output folder: "%WORK_FOLDERNAME%"
 
 POPD
-ENDLOCAL
-EXIT /B
+
+IF NOT "x-%EXIT_ERR%" == "x-" (
+    ECHO =======================
+    ECHO Output folder: "%WORK_FOLDERNAME%"
+
+    SET ERR=ERRORLEVEL=%EXIT_ERR%
+    IF NOT "x-%EXIT_ERR%" == "x-0" SET ERR=[91m!ERR![0m
+    ECHO !ERR!
+
+    ECHO =======================
+)
+
+ENDLOCAL & EXIT /B %EXIT_ERR%
--- nsTelemetry/nuget/SignPackage.cmd
+++ nsTelemetry/nuget/SignPackage.cmd
@@ -1,35 +1,80 @@
-@echo off
-REM Copyright 2020 nishy software
-SETLOCAL enabledelayedexpansion
+@ECHO off
+REM Copyright 2020, 2023 nishy software
+
+SETLOCAL EnableDelayedExpansion
+
 SET BAT_NAME=%~n0%~x0
-SET TARGET_FILE1=%1
+
+SET EXIT_ERR=0
 SET CERT_SUBJECT_NAME=nishy software
+SET TIME_STAMP_RFC3161_URL=http://timestamp.digicert.com/?alg=sha256
 
-IF "x-" == "x-%TARGET_FILE1%" GOTO HELP
+SET NO_PAUSE=
+IF "x-%1"=="x-/?" GOTO HELP
+IF "x-%1"=="x-/h" GOTO HELP
+IF "x-%1"=="x--h" GOTO HELP
+IF "x-%1"=="x--help" GOTO HELP
+IF "x-%1" == "x---no-pause" SET NO_PAUSE=true& SHIFT
+IF "x-%1" == "x--no-pause" SET NO_PAUSE=true& SHIFT
+IF "x-%1" == "x-/no-pause" SET NO_PAUSE=true& SHIFT
 
-REM  Signe packages
+SET TARGET_FILES=%*
+
+IF "x-" == "x-%TARGET_FILES%" GOTO HELP
+
+REM ------------------------
+REM  Sign packages
+REM ------------------------
 ECHO =======================
-ECHO Signe packages
+ECHO Sign packages
 ECHO -----------------------
-FOR %%i in (%*) do (
-    echo sign: %%~ni%%~xi
-    nuget.exe sign "%%i" -Verbosity quiet -CertificateSubjectName "%CERT_SUBJECT_NAME%" -Timestamper "http://sha256timestamp.ws.symantec.com/sha256/timestamp"
+FOR %%i in (%TARGET_FILES%) do (
+    ECHO sign: %%~ni%%~xi
+    nuget.exe sign "%%i" -Verbosity quiet -CertificateSubjectName "%CERT_SUBJECT_NAME%" -Timestamper "%TIME_STAMP_RFC3161_URL%"
+    IF ERRORLEVEL 1 GOTO ERROR
 )
 ECHO =======================
 
 GOTO END
 
+REM ------------------------
+REM :ERROR
+REM ------------------------
 :ERROR
-ECHO ERROR occurred
-COLOR C1
-PAUSE
-COLOR F
+
+SET EXIT_ERR=%ERRORLEVEL%
+ECHO [41m[1mERROR occurred (ERRORLEVEL=%EXIT_ERR%)[0m
+IF "x-%NO_PAUSE%" == "x-" (
+REM COLOR C1
+ECHO.[41m&PAUSE&ECHO [0m
+REM COLOR F
+)
+
 GOTO END
 
+REM ------------------------
+REM :HELP
+REM ------------------------
 :HELP
-ECHO %BAT_NAME% NugetPackageFiles
-ECHO Each file in NugetPackageFiles ends with .nupkg.
 
+ECHO.
+ECHO Usage: %BAT_NAME%  [--no-pause] nugetPackageFile1 [nugetPackageFile2 [...]]
+ECHO   remarks: Each file in NugetPackageFiles ends with .nupkg.
+ECHO.
+
+SET EXIT_ERR=
+
+GOTO END
+
+REM ------------------------
+REM :END
+REM ------------------------
 :END
-ENDLOCAL
-EXIT /B
+
+IF NOT "x-%EXIT_ERR%" == "x-" (
+    SET ERR=ERRORLEVEL=%EXIT_ERR%
+    IF NOT "x-%EXIT_ERR%" == "x-0" SET ERR=[91m!ERR![0m
+    ECHO !ERR!
+)
+
+ENDLOCAL & EXIT /B %EXIT_ERR%
